94608dcc604ff7a8194484938fde8b7c39f9d581
Duchess22 <liynenyago21@gmail.com>
2023-02-16 10:55:21 -0800

    all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main - use printf function
-*no puts
-*return 0
-*/
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
