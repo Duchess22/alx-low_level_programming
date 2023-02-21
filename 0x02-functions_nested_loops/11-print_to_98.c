f7e19f586daef22b48dbadec39814ce18b99d0fd
Duchess22 <liynenyago21@gmail.com>
2023-02-21 12:08:19 -0800

    to98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,8 @@
 * print_to_98 - Prints all natural numbers from n to 98
 * @n: The number to start printing from
 * Return: Always 0.
-*/void print_to_98(int n)
+*/void
+print_to_98(int n)
 {
 if (n <= 98)
 {
